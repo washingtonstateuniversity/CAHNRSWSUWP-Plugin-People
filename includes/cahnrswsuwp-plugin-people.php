a78b3d09493c4fa1103b02a2f4c2bf7f3033b600
danialbleile <danbleile@gmail.com>
2018-08-27 11:04:20 -0700

    Add WSU plugin skeleton files

---
+++ includes/cahnrswsuwp-plugin-people.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace WSUWP\CAHNRSWSUWP_Plugin_People;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+class CAHNRSWSUWP_Plugin_People {
+
+
+	public function __construct() {
+
+	}
+
+} // End CAHNRSWSUWP_Plugin_People
+
+$cahnrswsuwp_plugin_people = new CAHNRSWSUWP_Plugin_People();
